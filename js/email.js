85667d34891f3f8029684e16b269b7a6b722cffe
murphymUrph21 <iliveirep_831@yahoo.com>
2021-04-09 13:55:09 -0700

    Changed SweetAlert to only errors

--- js/email.js
+++ js/email.js
@@ -7,29 +7,13 @@ document.getElementById('form')
         const serviceID = 'default_service';
         const templateID = 'template_vjsmbci';
 
-   Swal.fire({
-             title: 'Do you want to save the changes?',
-                        showDenyButton: true,
-                        showCancelButton: true,
-                        confirmButtonText: `Send`,
-                        denyButtonText: `Don't send`,
-                    }).then((result) => {
-                        /* Read more about isConfirmed, isDenied below */
-                        if (result.isConfirmed) {
-                            Swal.fire('Sent!', '', 'success')
-                                 emailjs.sendForm(serviceID, templateID, this)
-                                     .then(() => {
-                                         btn.value = 'Send Email';
-
-                                     }, (err) => {
-                                         btn.value = 'Send Email';
-                                         Swal.fire('Message not Sent', '', JSON.stringify(err));
-                                     });
-                        } else if (result.isDenied) {
-                            Swal.fire('Message not Sent', '', 'info')
-                        }
-                    })
    
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+
+            }, (err) => {
+                Swal.fire('Message not Sent', '', JSON.stringify(err));
+            });
     });
 
                    
