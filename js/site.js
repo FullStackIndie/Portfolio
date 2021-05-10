39de1972042f5dbaf572fbf8ad083e61a4715045
murphymUrph21 <iliveirep_831@yahoo.com>
2021-05-10 15:52:55 -0700

    added modals for each challenge

---
+++ js/site.js
@@ -0,0 +1,73 @@
+
+
+function showTacoCat(){
+    $(function () {
+        $('#challenges').modal();
+    })
+
+    document.getElementById("challengeTitle").innerText = "Taco Cat";
+    document.getElementById("challengePicture").setAttribute("src", "/img/sereja-ris-TacoCat-unsplash.jpg");
+    document.getElementById("challengeSummary").innerText = 
+    "Javascript Challenge that implements an an algorithm to test if a word is a valid Palindrome";
+    document.getElementById("challengeLink").setAttribute("href", "https://jm-tacocat.netlify.app");
+}
+
+function showFizzBuzz() {
+    $(function () {
+        $('#challenges').modal();
+    })
+
+document.getElementById("challengeTitle").innerText = "Fizz Buzz";
+document.getElementById("challengePicture").setAttribute("src", "/img/sam-cernik-FizzBuzz-unsplash.jpg");
+document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that implements an an algorithm that displays multiples of any two numbers";
+document.getElementById("challengeLink").setAttribute("href", "https://jm-fizzbuzz.netlify.app");
+}
+
+function showSumOfAllFears(){
+    $(function () {
+        $('#challenges').modal();
+    })
+
+document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
+document.getElementById("challengePicture").setAttribute("src", "/img/roman-synkevych-SumOfAllFears-unsplash.jpg");
+document.getElementById("challengeSummary").innerText =
+    "Work iN progress";
+document.getElementById("challengeLink").setAttribute("href", "https://jm-tacocat.netlify.app");
+}
+
+function showMortgageCalculator(){
+    $(function () {
+        $('#challenges').modal();
+    })
+
+document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
+document.getElementById("challengePicture").setAttribute("src", "/img/mortgageCalcmid.png");
+document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan";
+document.getElementById("challengeLink").setAttribute("href", "https://jm-mortgagecalculator.netlify.app");
+}
+
+function showSuperDogEvents(){
+    $(function () {
+        $('#challenges').modal();
+    })
+
+document.getElementById("challengeTitle").innerText = "Super Dog events";
+document.getElementById("challengePicture").setAttribute("src", "/img/SuperDogEventsWebLogosmall.png");
+document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that sorts Events by City and by varying attendance";
+document.getElementById("challengeLink").setAttribute("href", "https://jm-superdogevents.netlify.app/");
+}
+
+function showTodoList(){
+    $(function () {
+        $('#challenges').modal();
+    })
+
+document.getElementById("challengeTitle").innerText = "Todo List";
+document.getElementById("challengePicture").setAttribute("src", "/img/todo-things.png");
+document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that track, edit, and sort tasks";
+document.getElementById("challengeLink").setAttribute("href", "https://jm-todolist.netlify.app");
+}
