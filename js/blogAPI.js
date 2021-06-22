4bae2c0901173a4daad7334203264bd69a971ebd
murphymUrph21 <iliveirep_831@yahoo.com>
2021-06-22 10:57:01 -0700

    added blog api functionality

---
+++ js/blogAPI.js
@@ -0,0 +1,42 @@
+$(function () {
+    fetch('//johnrockwellblog.herokuapp.com/GetTopXPosts/3')
+        .then(response => response.json())
+        .then(function (data) {
+
+            let blogUrl = "https://johnrockwellblog.herokuapp.com/BlogPosts/Details/";
+
+            let options = {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            }
+
+
+            let blogLink = `${blogUrl}${data[0].slug}`;
+            var blogPosts = document.getElementById('blog-posts');
+            data.forEach(function (post) {
+                var postCard =
+                    `<div class="card m-lg-3" style="width: 18rem;">
+                        <a href="${blogUrl}${post.slug}" target="_blank">
+                        <img class="card-img-top" src="data:${post.contentType};base64,${post.imageData}" alt="Card image cap" />
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${post.abstract}</p>
+                            <a target="_blank" class="btn btn-light">Check it out</a>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Created: ${new Date(post.created).toLocaleDateString("en-US", options)}
+                        </div>
+                    </div>
+                `;
+                blogPosts.insertAdjacentHTML('beforeend', postCard);
+            });
+
+
+
+
+        })
+
+});
