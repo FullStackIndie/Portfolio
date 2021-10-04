3a0e2c854d1bda62c4014095408b00fbd3fc3b83
murphymUrph21 <iliveirep_831@yahoo.com>
2021-10-04 13:44:21 -0700

    updated urls for portfolio

--- js/site.js
+++ js/site.js
@@ -1,15 +1,13 @@
-
-
-function showTacoCat(){
+function showTacoCat() {
     $(function () {
         $('#challenges').modal();
     })
 
     document.getElementById("challengeTitle").innerText = "Taco Cat";
     document.getElementById("challengePicture").setAttribute("src", "/img/sereja-ris-TacoCat-unsplash.jpg");
-    document.getElementById("challengeSummary").innerText = 
-    "Javascript Challenge that implements an algorithm to test if a word is a valid Palindrome.";
-    document.getElementById("challengeLink").setAttribute("href", "https://tacocat.coding4christ.net/");
+    document.getElementById("challengeSummary").innerText =
+        "Javascript Challenge that implements an algorithm to test if a word is a valid Palindrome.";
+    document.getElementById("challengeLink").setAttribute("href", "https://tacocat.fullstackindie.net/");
 }
 
 function showFizzBuzz() {
@@ -17,57 +15,57 @@ function showFizzBuzz() {
         $('#challenges').modal();
     })
 
-document.getElementById("challengeTitle").innerText = "Fizz Buzz";
-document.getElementById("challengePicture").setAttribute("src", "/img/sam-cernik-FizzBuzz-unsplash.jpg");
-document.getElementById("challengeSummary").innerText =
-    "Javascript Challenge that implements an algorithm that displays multiples of any two numbers.";
-document.getElementById("challengeLink").setAttribute("href", "https://fizzbuzz.coding4christ.net/");
+    document.getElementById("challengeTitle").innerText = "Fizz Buzz";
+    document.getElementById("challengePicture").setAttribute("src", "/img/sam-cernik-FizzBuzz-unsplash.jpg");
+    document.getElementById("challengeSummary").innerText =
+        "Javascript Challenge that implements an algorithm that displays multiples of any two numbers.";
+    document.getElementById("challengeLink").setAttribute("href", "https://fizzbuzz.fullstackindie.net/");
 }
 
-function showSumOfAllFears(){
+function showSumOfAllFears() {
     $(function () {
         $('#challenges').modal();
     })
 
-document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
-document.getElementById("challengePicture").setAttribute("src", "/img/roman-synkevych-SumOfAllFears-unsplash.jpg");
-document.getElementById("challengeSummary").innerText =
-    "JavaScript Challenge that looks for any sum of two numbers inside of an array that equals the number were looking for.";
-document.getElementById("challengeLink").setAttribute("href", "https://sumofallfears.coding4christ.net/");
+    document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
+    document.getElementById("challengePicture").setAttribute("src", "/img/roman-synkevych-SumOfAllFears-unsplash.jpg");
+    document.getElementById("challengeSummary").innerText =
+        "JavaScript Challenge that looks for any sum of two numbers inside of an array that equals the number were looking for.";
+    document.getElementById("challengeLink").setAttribute("href", "https://sumofallfears.fullstackindie.net/");
 }
 
-function showMortgageCalculator(){
+function showMortgageCalculator() {
     $(function () {
         $('#challenges').modal();
     })
 
-document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
-document.getElementById("challengePicture").setAttribute("src", "/img/mortgageCalcmid.png");
-document.getElementById("challengeSummary").innerText =
-    "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan.";
-document.getElementById("challengeLink").setAttribute("href", "https://mortgagecalculator.coding4christ.net/");
+    document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
+    document.getElementById("challengePicture").setAttribute("src", "/img/mortgageCalcmid.png");
+    document.getElementById("challengeSummary").innerText =
+        "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan.";
+    document.getElementById("challengeLink").setAttribute("href", "https://mortgagecalculator.fullstackindie.net/");
 }
 
-function showSuperDogEvents(){
+function showSuperDogEvents() {
     $(function () {
         $('#challenges').modal();
     })
 
-document.getElementById("challengeTitle").innerText = "Super Dog events";
-document.getElementById("challengePicture").setAttribute("src", "/img/superdogevents.webp");
-document.getElementById("challengeSummary").innerText =
-    "Javascript Challenge that sorts local events by city and by varying attendance.";
-document.getElementById("challengeLink").setAttribute("href", "https://superdogevents.coding4christ.net/");
+    document.getElementById("challengeTitle").innerText = "Super Dog events";
+    document.getElementById("challengePicture").setAttribute("src", "/img/superdogevents.webp");
+    document.getElementById("challengeSummary").innerText =
+        "Javascript Challenge that sorts local events by city and by varying attendance.";
+    document.getElementById("challengeLink").setAttribute("href", "https://superdogevents.fullstackindie.net/");
 }
 
-function showTodoList(){
+function showTodoList() {
     $(function () {
         $('#challenges').modal();
     })
 
-document.getElementById("challengeTitle").innerText = "Todo List";
-document.getElementById("challengePicture").setAttribute("src", "/img/todo-things.png");
-document.getElementById("challengeSummary").innerText =
-    "To-do list that lets you track, edit, and sort tasks.";
-document.getElementById("challengeLink").setAttribute("href", "https://todolist.coding4christ.net/");
+    document.getElementById("challengeTitle").innerText = "Todo List";
+    document.getElementById("challengePicture").setAttribute("src", "/img/todo-things.png");
+    document.getElementById("challengeSummary").innerText =
+        "To-do list that lets you track, edit, and sort tasks.";
+    document.getElementById("challengeLink").setAttribute("href", "https://todolist.fullstackindie.net/");
 }
