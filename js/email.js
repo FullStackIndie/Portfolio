e3ac6e961512af932b77d90a7ea44e3b7c6b13f0
murphymUrph21 <johnmurphy_21@yahoo.com>
2023-05-07 11:48:47 -0700

    Added new SweetAlert
    for when sending an email and catching errors

--- js/email.js
+++ js/email.js
@@ -1,17 +1,31 @@
-const btn = document.getElementById('button');
+const btn = document.getElementById("button");
 
-document.getElementById('form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-        const serviceID = 'service_rl2adad';
-        const templateID = 'template_yjm3hz9';
+  const serviceID = "service_rl2adad";
+  const templateID = "template_yjm3hz9";
 
-   
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                Swal.fire('Message Sent');
-            }, (err) => {
-                Swal.fire('Message not Sent', '', JSON.stringify(err));
-            });
-    });
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      Swal.fire("Message Sent");
+    },
+    (err) => {
+            console.log(err);
+            console.log(JSON.stringify(err));
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong! Message was not Sent",
+        footer:
+          `<button data-error='${JSON.stringify(err)}' onclick="displayError(this)">Display issue</button>`,
+      });
+    }
+  );
+});
+
+function displayError(elem){
+    let error = elem.getAttribute('data-error');
+     Swal.fire(`${error}`);
+     console.log(error);
+}
