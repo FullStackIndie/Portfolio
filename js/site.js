93ee28ca0a9ecde44748c9103205a940815fca94
murphymUrph21 <johnmurphy_21@yahoo.com>
2023-05-07 22:58:35 -0700

    updated links for modals

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ function showTacoCat() {
     })
 
     document.getElementById("challengeTitle").innerText = "Taco Cat";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/sereja-ris-TacoCat-unsplash.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sereja-ris-TacoCat-unsplash.webp");
     document.getElementById("challengeSummary").innerText =
         "Javascript Challenge that implements an algorithm to test if a word is a valid Palindrome.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/taco-cat");
@@ -17,7 +17,7 @@ function showFizzBuzz() {
     })
 
     document.getElementById("challengeTitle").innerText = "Fizz Buzz";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/sam-cernik-FizzBuzz-unsplash.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sam-cernik-FizzBuzz-unsplash.webp");
     document.getElementById("challengeSummary").innerText =
         "Javascript Challenge that implements an algorithm that displays multiples of any two numbers.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/fizz-buzz");
@@ -29,7 +29,7 @@ function showSumOfAllFears() {
     })
 
     document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/roman-synkevych-SumOfAllFears-unsplash.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/roman-synkevych-SumOfAllFears-unsplash.webp");
     document.getElementById("challengeSummary").innerText =
         "JavaScript Challenge that looks for any sum of two numbers inside of an array that equals the number were looking for.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/sum-of-all-fears");
@@ -41,7 +41,7 @@ function showMortgageCalculator() {
     })
 
     document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/mortgageCalcmid.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/mortgageCalcmid.webp");
     document.getElementById("challengeSummary").innerText =
         "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/mortgage-calculator");
@@ -53,7 +53,7 @@ function showSuperDogEvents() {
     })
 
     document.getElementById("challengeTitle").innerText = "Super Dog events";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/superdogevents.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/superdogevents.webp");
     document.getElementById("challengeSummary").innerText =
         "Javascript Challenge that sorts local events by city and by varying attendance.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/super-dog-events");
@@ -65,7 +65,7 @@ function showTodoList() {
     })
 
     document.getElementById("challengeTitle").innerText = "Todo List";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/img/todo-things.webp");
+    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/todo-things.webp");
     document.getElementById("challengeSummary").innerText =
         "To-do list that lets you track, edit, and sort tasks.";
     document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/todo-list");
