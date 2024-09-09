0ff98109bed1899c864e34fccef91c10d245f252
FullStackIndie <johnmurphy_21@yahoo.com>
2024-09-08 21:17:26 -0700

    added dynamic urls for js scripts. added portfolio and social website images. added dynamic years for copyright.

--- js/blogAPI.js
+++ js/blogAPI.js
@@ -1,45 +1,46 @@
 $(function () {
-    fetch('//blog.fullstackindie.net/GetTopXPosts/3')
-        .then(response => response.json())
-        .then(function (data) {
+  fetch("//blog.fullstackindie.net/GetTopXPosts/3")
+    .then((response) => response.json())
+    .then(function (data) {
+      let blogUrl = "https://blog.fullstackindie.net/BlogPosts/Details/";
 
-            let blogUrl = "https://blog.fullstackindie.net/BlogPosts/Details/";
-
-            let options = {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            }
-
-            console.log(data[0]);
-            let blogLink = `${blogUrl}${data[0].slug}`;
-            var blogPosts = document.getElementById('blog-posts');
-            data.forEach(function (post) {
-                var postCard =
-                    `<div class="card m-lg-3" style="width: 18rem;">
+      let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      if (data !== null || data !== undefined) {
+        document.getElementById("blog-posts").style.display = block;
+      }
+      console.log(data[0]);
+      let blogLink = `${blogUrl}${data[0].slug}`;
+      var blogPosts = document.getElementById("blog-posts");
+      data.forEach(function (post) {
+        var postCard = `<div class="card m-lg-3" style="width: 18rem;">
                         <a href="${blogUrl}${post.slug}" target="_blank">
-                        <img class="card-img-top" src="data:${post.contentType};base64,${post.imageData}" alt="Card image cap" />
+                        <img class="card-img-top" src="data:${
+                          post.contentType
+                        };base64,${post.imageData}" alt="Card image cap" />
                         </a>
                         <div class="card-body">
                             <h5 class="card-title">${post.title}</h5>
                             <p class="card-text">${post.abstract}</p>
-                            <a target="_blank" href="${blogUrl}${post.slug}" class="btn btn-light">Check it out</a>
+                            <a target="_blank" href="${blogUrl}${
+          post.slug
+        }" class="btn btn-light">Check it out</a>
                         </div>
                         <div class="card-footer text-muted">
-                            Created: ${new Date(post.created).toLocaleDateString("en-US", options)}
+                            Created: ${new Date(
+                              post.created
+                            ).toLocaleDateString("en-US", options)}
                         </div>
                     </div>
                 `;
-                blogPosts.insertAdjacentHTML('beforeend', postCard);
-            });
-
-
-
-
-        }).catch((err) => 
-        {
-            console.log(err);
-        });
-
+        blogPosts.insertAdjacentHTML("beforeend", postCard);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
--- js/site.js
+++ js/site.js
@@ -1,73 +1,142 @@
-
 function showTacoCat() {
-    javascript: void(0);
-    $(function () {
-        $('#challenges').modal();
-    })
+  javascript: void 0;
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Taco Cat";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sereja-ris-TacoCat-unsplash.webp");
-    document.getElementById("challengeSummary").innerText =
-        "Javascript Challenge that implements an algorithm to test if a word is a valid Palindrome.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/taco-cat");
+  document.getElementById("challengeTitle").innerText = "Taco Cat";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sereja-ris-TacoCat-unsplash.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that implements an algorithm to test if a word is a valid Palindrome.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/taco-cat"
+    );
 }
 
 function showFizzBuzz() {
-    $(function () {
-        $('#challenges').modal();
-    })
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Fizz Buzz";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sam-cernik-FizzBuzz-unsplash.webp");
-    document.getElementById("challengeSummary").innerText =
-        "Javascript Challenge that implements an algorithm that displays multiples of any two numbers.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/fizz-buzz");
+  document.getElementById("challengeTitle").innerText = "Fizz Buzz";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/sam-cernik-FizzBuzz-unsplash.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that implements an algorithm that displays multiples of any two numbers.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/fizz-buzz"
+    );
 }
 
 function showSumOfAllFears() {
-    $(function () {
-        $('#challenges').modal();
-    })
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/roman-synkevych-SumOfAllFears-unsplash.webp");
-    document.getElementById("challengeSummary").innerText =
-        "JavaScript Challenge that looks for any sum of two numbers inside of an array that equals the number were looking for.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/sum-of-all-fears");
+  document.getElementById("challengeTitle").innerText = "Sum Of All Fears";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/roman-synkevych-SumOfAllFears-unsplash.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "JavaScript Challenge that looks for any sum of two numbers inside of an array that equals the number were looking for.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/sum-of-all-fears"
+    );
 }
 
 function showMortgageCalculator() {
-    $(function () {
-        $('#challenges').modal();
-    })
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/mortgageCalcmid.webp");
-    document.getElementById("challengeSummary").innerText =
-        "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/mortgage-calculator");
+  document.getElementById("challengeTitle").innerText = "Mortgage Calculator";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/mortgageCalcmid.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that uses a formula to calculate monthly costs of a mortgage loan.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/mortgage-calculator"
+    );
 }
 
 function showSuperDogEvents() {
-    $(function () {
-        $('#challenges').modal();
-    })
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Super Dog events";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/superdogevents.webp");
-    document.getElementById("challengeSummary").innerText =
-        "Javascript Challenge that sorts local events by city and by varying attendance.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/super-dog-events");
+  document.getElementById("challengeTitle").innerText = "Super Dog events";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/superdogevents.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "Javascript Challenge that sorts local events by city and by varying attendance.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/super-dog-events"
+    );
 }
 
 function showTodoList() {
-    $(function () {
-        $('#challenges').modal();
-    })
+  $(function () {
+    $("#challenges").modal();
+  });
 
-    document.getElementById("challengeTitle").innerText = "Todo List";
-    document.getElementById("challengePicture").setAttribute("src", "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/todo-things.webp");
-    document.getElementById("challengeSummary").innerText =
-        "To-do list that lets you track, edit, and sort tasks.";
-    document.getElementById("challengeLink").setAttribute("href", "https://portfolio.fullstackindie.net/challenges/todo-list");
+  document.getElementById("challengeTitle").innerText = "Todo List";
+  document
+    .getElementById("challengePicture")
+    .setAttribute(
+      "src",
+      "https://dsm56ob6rdulz.cloudfront.net/Portfolio/img/todo-things.webp"
+    );
+  document.getElementById("challengeSummary").innerText =
+    "To-do list that lets you track, edit, and sort tasks.";
+  document
+    .getElementById("challengeLink")
+    .setAttribute(
+      "href",
+      "https://portfolio.fullstackindie.net/challenges/todo-list"
+    );
 }
+
+function getYear() {
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
+  document.getElementById(
+    "copyright"
+  ).innerText = `Copyright © ${currentYear} - John Murphy All Rights Reserved.`;
+}
+
+getYear();
